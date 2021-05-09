e65505ddeaa952ea6a9973ab7bc9e0f8ce724445
Bozîeac Artur <39851804+Chandler1215@users.noreply.github.com>
2021-05-09 03:51:15 +0300

    Basic UI, 3 screens and game logic for Guess number game

---
+++ 3.GuessNumberApp/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import GameOverScreen from './screens/GameOverScreen';
+import GameScreen from './screens/GameScreen';
+import StartGameScreen from './screens/StartGameScreen';
+
+
+export default function App() {
+  const[userNumber, setUserNumber] = useState();
+  const[guessRounds, setGuessRounds] = useState(0);
+
+  const newGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
+
+  const startGameHandler = (selectedNumber) =>{
+    setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+  if(userNumber && guessRounds <= 0){
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>;
+  }else if(guessRounds > 0){
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={newGameHandler}/>;
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number"/>
+      {content}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
---
+++ 3.GuessNumberApp/screens/GameOverScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const GameOverScreen = props => {
+    return <View style={styles.screen}>
+        <Text>Game is over !</Text>
+        <Text> Number of rounds: {props.roundsNumber}</Text>
+        <Text> Number was: {props.userNumber}</Text>
+        <Button title="NEW GAME" onPress={props.onRestart}></Button>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+    scren:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default GameOverScreen;
